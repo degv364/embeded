497ff84dbb0c28445dd185a3ffe74c96151ceb18
Esteban Zamora <estebanzacr.20@gmail.com>
2017-10-02 22:39:43 -0600

    Reestructured Lab2 project with Lab1 template

---
+++ Lab2/include/hi/hi_def.hh
@@ -0,0 +1,48 @@
+/**
+ Copyright 2017 Daniel Garcia Vaglio <degv364@gmail.com> Esteban Zamora Alvarado <estebanzacr.20@gmail.com>
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ **/
+
+#ifndef TESTING
+#include <stdint.h>
+#endif
+
+#ifndef HI_DEF_H_
+#define HI_DEF_H_
+
+// States
+typedef enum hi_state_e
+{
+    HI_STATE_NONE = 0,   // No state
+    HI_STATE_INIT,       // Initialize peripherals
+    HI_STATE_ALIVE_SEQ,  // Functioning confirmation
+    HI_STATE_ON,         // Lamp ON
+    HI_STATE_OFF,        // Lamp OFF
+    HI_STATE_DEINIT,     // Deinitialize
+    HI_STATE_FAIL,       // Failure occurred
+    HI_STATE_MANUAL_ON   // Manual button ON state
+} hi_state_e;
+
+typedef struct hi_sensor_t
+{
+    uint16_t time;       //Software time counter (managed by Timer32)
+    bool control_button; //Manual control button activation flag
+    bool light_sensor;   //Light sensor condition (high level=True)
+    bool microphone;     //Microphone loud flag (loud=True)
+} hi_sensor_t;
+
+#endif
---
+++ Lab2/src/hd/isr.cpp
@@ -0,0 +1,65 @@
+/**
+ Copyright 2017 Daniel Garcia Vaglio <degv364@gmail.com> Esteban Zamora Alvarado <estebanzacr.20@gmail.com>
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ **/
+
+/* Standard Includes */
+#include <ti/devices/msp432p4xx/driverlib/driverlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "msp.h"
+
+/* Project Includes*/
+#include "common_def.hh"
+#include "hd/periph.hh"
+#include "hi/hi_def.hh"
+#include "hi/hi_utils.hh"
+
+/*Variables defined in other files*/
+
+
+/*Interrupt Service Routines (ISR) Definition*/
+extern "C"
+{
+
+/* ISR that manages the microphone reading. Stores the current
+ * ADC sample (after some processing) in the microphone circular
+ * buffer for later analysis
+ */
+void ADC14_IRQHandler(void)
+{
+    __disable_irq();
+    //FIXME: Implement ADC ISR
+
+    __enable_irq();
+}
+
+/* ISR activated by Timer32 for time measurement through the
+ * software timer update
+*/
+void T32_INT1_IRQHandler(void)
+{
+    __disable_irq();
+
+    periph::Timer::cleanIRQ(TIMER32_0_BASE);
+    //FIXME: Implement T32 ISR
+
+    __enable_irq();
+}
+
+}
+
